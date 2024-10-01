c75a43c6db9ebf9e9079dfefa2fc4b34cdf8b183
fellpos <ra54399754893@acaonsfatima.org.br>
2024-10-01 15:00:52 -0300

    vai dar tudo errado

--- src/pages/usuario/home/index.jsx
+++ src/pages/usuario/home/index.jsx
@@ -7,7 +7,8 @@ export default function Home() {
   return (
     <div className="pagina-home">
       <section className='secao-1'>
-
+      <img className='fundo' src="/assets/images/back-home.png" alt="" />
+      
         <div className="cabecalho">
 
           <div class="logo">
