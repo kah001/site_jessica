cdf0541fda20ff9c0bb0d08e5b66870c58437a6a
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-01 14:56:38 -0300

    Mario

---
+++ src/components/rodape/index.jsx
@@ -0,0 +1,27 @@
+import './index.scss';
+
+
+
+export default function Rodapé () {
+
+
+
+
+    return (
+        <div className='Rodapé'>
+            <div className="Links">
+                <div className="Paginas">
+
+                </div>
+                <img className='Logo' src="" alt="Logo Jessica Lima Arquitetura" />
+                <div className="Minha-Conta">
+
+                </div>
+            </div>
+            <div className="Linha-Branca"></div>
+            <div className="Icones-Contato">
+
+            </div>
+        </div>
+    )
+}
