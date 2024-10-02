f3c7d1d67cc460fdee407fc967e44f3d27994168
fellpos <ra54399754893@acaonsfatima.org.br>
2024-10-02 03:56:15 -0300

    finalizado caebcalho home, tentando fazer o bloco de texto ficar por cima da imagem

--- src/pages/usuario/home/index.jsx
+++ src/pages/usuario/home/index.jsx
@@ -1,43 +1,73 @@
-import Rodape from '../../../components/rodape';
 import './index.scss';
+import Rodape from '../../../components/rodape';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 
 
-  return (
-    <div className="pagina-home">
+    return (
+        <div className="pagina-home" >
 
-      <section className='secao-1'>
-      
-        <div className="cabecalho">
-          <div class="logo">
-            <img src="/assets/images/logo-tcc.png" alt="" height="80px" />
-          </div>
+            <section className='secao-1'>
 
-          <div class="item">
-            <a href="">Início</a>
-          </div>
-          <div class="item">
-            <a href="">Sobre Mim</a>
-          </div>
-          <div class="item">
-            <a href="">Projetos</a>
-          </div>
+                <div className="cabecalho">
 
-          <div class="item">
-            <a href="">Dicas</a>
-          </div>
+                    <div class="logo">
+                        <Link to='/'>
+                            <img src="/assets/images/logo-tcc.png" alt="" height="80px" />
+                        </Link>
+                    </div>
 
-          <div class="item">
-            <a href="">Entrar</a>
-          </div>
-          <div class="item">
-            <img src="/assets/images/person-icon.png" alt="" height="50px" />
-          </div>
-        </div>
+                    <div class="item">
+                        <Link to='/'>
+                            <a href="">Início</a>
+                        </Link>
+                    </div>
 
-        <Rodape/>
-      </section>
-    </div >
-  );
+                    <div class="item">
+                        <Link to='/Sobre-Mim'>
+                            <a href="">Sobre Mim</a>
+                        </Link>
+                    </div>
+
+                    <div class="item">
+                        <Link to='/Projetos'>
+                            <a href="">Projetos</a>
+                        </Link>
+                    </div>
+
+                    <div class="item">
+                        <Link to='/Dicas'>
+                            <a href="">Dicas</a>
+                        </Link>
+                    </div>
+
+                    <div class="item">
+                        <Link to='/Entrar'>
+                            <a href="">Entrar</a>
+                        </Link>
+                    </div>
+
+                    <div class="item">
+                        <Link to='/Entrar'>
+                            <img src="/assets/images/person-icon.png" alt="" height="50px" />
+                        </Link>
+                    </div>
+
+                </div>
+                <img className='background' src="/assets/images/back-home.png" alt="" />
+
+                <div className="text-block">
+                    <div className="text">
+
+                    <h1>Construa sua casa com harmonia</h1>
+
+                    <h2>Planejamento, previsão de gastos e cronograma da obra</h2>
+                    </div>
+                </div>
+
+                <Rodape />
+            </section>
+        </div >
+    );
 }
