ae34b401aeb0661595e21266510b08bf035aed06
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-14 16:35:08 -0300

    alterações apenas no jsx e no css do rodapé

--- src/components/rodape/index.jsx
+++ src/components/rodape/index.jsx
@@ -7,12 +7,18 @@ export default function Rodape() {
         <div className='comps-rodape'>
             <div className="Links">
                 <div className="Paginas">
-                    <h1>Páginas</h1>
-                    <a href="">&nbsp;</a>
+                    <h1 className='Titulo1'>Páginas</h1>
+                    <a className='Link' href=""> Início </a>
+                    <a className='Link' href=""> Sobre Mim </a>
+                    <a className='Link' href=""> Dicas </a>
+                    <a className='Link' href=""> Projetos </a>
                 </div>
                 <div className="Minha-Conta">&nbsp;</div>
             </div>
-            <div className="Linha-Branca">&nbsp;</div>
+            <div className='Espaço-Linha'>
+                <div className="Linha-Branca">&nbsp;</div>
+            </div>
+            
             <div className="Icones-Contato">&nbsp;</div>
         </div>
     )
