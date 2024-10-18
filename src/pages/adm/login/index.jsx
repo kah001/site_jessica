9cc4b0c5cee6d0c612be5629473af5cbb7e448d9
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-18 14:42:30 -0300

    alterações no rodapé, e na pagina de login

---
+++ src/pages/adm/login/index.jsx
@@ -0,0 +1,15 @@
+import './index.scss';
+
+
+export default function Login (){
+
+
+
+    return (
+        <div className='body'>
+            <section className='login'>
+                
+            </section>
+        </div>
+    )
+}
