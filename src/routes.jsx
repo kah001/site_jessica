9a671f4d710c933067e377f5dae64b6b6d4a62e2
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-18 16:40:27 -0300

    alterações na pagina login, e erro corrigido do rodapé

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/usuario/home/";
+import Login from "./pages/adm/login";
 
 export default function Navegacao() {
 
@@ -7,6 +8,7 @@ export default function Navegacao() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/Login" element={<Login />}/>
             </Routes>
         </BrowserRouter>
     )
