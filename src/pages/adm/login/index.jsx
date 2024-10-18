def47e236b024242ed642256f54e1060728efbb9
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-18 17:57:03 -0300

    alterações no login, erro do rodapé corrigido

--- src/pages/adm/login/index.jsx
+++ src/pages/adm/login/index.jsx
@@ -7,22 +7,27 @@ export default function Login (){
 
     return (
         <div className='pagina-login'>
-            
+
+        <img className='fundo' src="/assets/images/image-login.png" alt=""/>            
             <section className='login'>
                 <section className='espaço-seta'>
                     <img className='seta' src="/assets/images/seta-login.png" alt=""/>
                 </section>
+
                 <h2 className='titulo'>Seja muito Bem-Vindo(a)!</h2>
+
                 <section className='inputs'>
                     <section className='input1'>
                         <p>Nome:</p>
-                        <input className='nome' type="text" placeholder='nome de usuario'/>
+                        <input className='nome' type="text" />
                     </section>
+
                     <section className='input2'>
                         <p>Senha:</p>
-                        <input className='senha' type="text" placeholder='senha'/>
+                        <input className='senha' type="password" />
                     </section>
                 </section>
+                
                 <button className='entrar'>Entrar</button>
             </section>
         </div>
