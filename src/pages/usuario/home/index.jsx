becea85a0c0dd748479b9acb11d7cd2607fe4a2f
kahvicente <karinasilvavicente007@gmail.com>
2024-10-21 01:29:22 -0300

    verificando menu responsivo (nao consegui arrumar, o menu ainda fica embaixo do carrossel, depois olho com mais calma)

--- src/pages/usuario/home/index.jsx
+++ src/pages/usuario/home/index.jsx
@@ -24,10 +24,10 @@ export default function Home() {
                     <nav className={`nav ${isOpen ? 'open' : ''}`}>
 
                         <div class="item"><Link to='/'><a href="">Início</a></Link></div>
-                        <div class="item"><Link to='/Sobre-Mim'><a href="">Sobre Mim</a></Link></div>
-                        <div class="item"><Link to='/Projetos'><a href="">Projetos</a></Link></div>
-                        <div class="item"><Link to='/Dicas'><a href="">Dicas</a></Link></div>
-                        <div class="item"><Link to='/Login'><img src="/assets/images/person-icon.png" alt="" height="50px" /></Link></div>
+                        <div class="item"><Link to='/sobre'><a href="">Sobre Mim</a></Link></div>
+                        <div class="item"><Link to='/projetos'><a href="">Projetos</a></Link></div>
+                        <div class="item"><Link to='/dicas'><a href="">Dicas</a></Link></div>
+                        <div class="item"><Link to='/login'><img src="/assets/images/person-icon.png" alt="" height="50px" /></Link></div>
                     </nav>
 
                     <div className='menu-icon' onClick={toggleMenu}>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/usuario/home/";
+import Home from "./pages/usuario/home";
 import Login from "./pages/adm/login";
 import Dicas from "./pages/usuario/dicas";
 import Projetos from "./pages/usuario/projetos";
@@ -8,15 +8,14 @@ import ProjetosAdm from "./pages/adm/projetos";
 import NaoEncontrado from "./pages/usuario/naoEncontrado";
 
 export default function Navegacao() {
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/dicas" element={<Dicas />} />
-                <Route path="/login" element={<Login />}/>
-                <Route path="/projetos" element={<Projetos />}/>
-                <Route path="/projetos/adm" element={<ProjetosAdm />}/>
+                <Route path="/login" element={<Login />} />
+                <Route path="/projetos" element={<Projetos />} />
+                <Route path="/adm/projetos" element={<ProjetosAdm />} />
 
                 <Route path="*" element={<NaoEncontrado />}/>
             </Routes>
