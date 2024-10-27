08e9e324c7e4f5250cb5e1782ab86f98dfe59ba1
fellpos <ra54399754893@acaonsfatima.org.br>
2024-10-27 01:15:31 -0300

    não conseguindo chamar/renderizar a imagem da api

--- src/pages/adm/alteracoes/index.jsx
+++ src/pages/adm/alteracoes/index.jsx
@@ -9,6 +9,10 @@ import axios from 'axios';
 export default function AlteracoesAdm() {
     const [token, setToken] = useState(null);
 
+    const [imagemRecente, setImagemRecente] = useState('');
+    const [tipoRecente, setTipoRecente] = useState('');
+    const [localRecente, setLocalRecente] = useState('');
+
     const navigate = useNavigate();
 
     const [imagem, setImagem] = useState(null);
@@ -28,7 +32,9 @@ export default function AlteracoesAdm() {
         }
     }
 
-    async function inserirProjetoAndamento() {
+    async function inserir(e) {
+        e.preventDefault() // faz a tela n recarregar quando insere
+
         const paramObj = {
             imagem: imagem,
             tipo: tipo,
@@ -39,22 +45,43 @@ export default function AlteracoesAdm() {
         let response = await axios.post(url, paramObj);
         let id = response.data.novoId
 
-        alert(`Novo Projeto em andamento adcionado. Id: ${id}`)
+        alert(`Novo Projeto em andamento adcionado. Id: ${id}`);
+        setImagem(null);
+        setTipo('');
+        setLocal('');
+        consultar();
+    }
+
+    async function consultar(token) {
+        try {
+            const url = `http://localhost:5010/projeto/andamento/recente?x-access-token=${token}`;
+            const resp = await axios.get(url);
+            let dados = resp.data
+            console.log(dados)
+
+            setImagemRecente(dados.imagem)
+            setTipoRecente(dados.tipo)
+            setLocalRecente(dados.local)
+        }
+        catch (error) {
+            console.error('Erro ao consultar:', error.message);
+        }
     }
 
     useEffect(() => {
-        let token = localStorage.getItem('USUARIO');
+        const token = localStorage.getItem('USUARIO');
         setToken(token);
 
-        if (token == 'null' || token == undefined) {
+        if (token === 'null' || token === undefined) {
             navigate('/')
         }
-    }, [])
+        consultar(token)
+    }, [navigate])
 
     return (
         <div className='pagina-alteracoes-adm'>
             <CabecalhoAdm />
-            <div style={{backgroundColor: "#D9D9D9", height: "50px"}}/>
+            <div style={{ backgroundColor: "#D9D9D9", height: "50px" }} />
 
 
             <section className='secao-01'>
@@ -68,29 +95,32 @@ export default function AlteracoesAdm() {
                         <div className='image-box'>
                             {imagem &&
                                 <img
-                                src={imagem}
-                                alt="Imagem"
                                     className='imagem'
-                                    />
+                                    src={imagem}
+                                    alt="Imagem"
+                                />
                             }
 
                             {imagem == null ?
                                 <label for="arquivo" style={{
                                     display: "flex",
                                     flexDirection: 'column',
-                                    alignItems: 'center'
-                                }}>
+                                    alignItems: 'center',
+                                    cursor: 'pointer'
+                                }} >
                                     <i class="fa-solid fa-image fa-flip-horizontal" style={{ fontSize: '70px' }} />
-                                    <label style={{ fontSize: '25px' }}>IMAGEM</label>
+
+                                    <label style={{ fontSize: '25px', cursor: 'pointer' }}>IMAGEM</label>
                                 </label>
                                 :
                                 <label for="arquivo" className='img-icon' style={{
                                     display: "flex",
                                     flexDirection: 'column',
-                                    alignItems: 'center'
+                                    alignItems: 'center',
+                                    cursor: 'pointer'
                                 }}>
                                     <i class="fa-solid fa-image fa-flip-horizontal" style={{ fontSize: '70px', color: '#fff' }}></i>
-                                    <label for="arquivo" style={{ fontSize: '25px', color: '#fff' , fontWeight: '700'}}>ALTERAR</label>
+                                    <label for="arquivo" style={{ fontSize: '25px', color: '#fff', fontWeight: '700', cursor: 'pointer' }}>ALTERAR</label>
                                 </label>
                             }
                             <input
@@ -105,52 +135,59 @@ export default function AlteracoesAdm() {
                         <div className='text-box'>
 
                             <div className='text'>
-
                                 <div>
                                     <label> TIPO: </label>
-                                    <input type="text" value={tipo} onChange={e => setTipo(e.target.value)} />
+                                    <input type="text" value={tipo} onChange={e => setTipo(e.target.value)} placeholder='Casas ou Apartamentos...' />
                                 </div>
 
                                 <div>
                                     <label> LOCAL: </label>
-                                    <input type="text" value={local} onChange={e => setLocal(e.target.value)} />
+                                    <input type="text" value={local} onChange={e => setLocal(e.target.value)} placeholder='Cidade, País...' />
                                 </div>
                             </div>
 
                             <div className="button">
-                                <button onClick={inserirProjetoAndamento}> INSERIR </button>
+                                <button onClick={inserir} required> INSERIR </button>
                             </div>
                         </div>
                     </div>
                 </form>
-                                <div style={{backgroundColor: "#D9D9D9", height: "50px"}}   />  
+                <div style={{ backgroundColor: "#D9D9D9", height: "50px" }} />
             </section>
 
             <section className='secao-02'>
+
+
                 <div className='titulo'>
                     <h1>PROJETO EM ANDAMENTO</h1>
                 </div>
 
-                <div className='-imagem'>
-                    {imagem &&
-                        <img
-                            className=''
-                            src={imagem}
-                            alt="Foto"
-                        />
-                    }
-                </div>
+                <div className='form'>
 
-                <div>
-                    <p>Tipo (casas ou apartamentos)</p>
+                    <div className="box">
 
-                    <h2>Local (cidade, pais)</h2>
-                </div>
 
-                <div>
-                    <i className="fa-solid fa-trash-can"></i>
+                        <div className="">
 
-                    <i className="fa-solid fa-pen-to-square"></i>
+                            <div className='image-box'>
+
+                                <img className='imagem' src={imagemRecente} alt={imagemRecente} />
+                            </div>
+                            <div><h2>TIPO {tipoRecente}</h2>
+                            </div>
+                            <div>
+                                <h1>LOCAL {localRecente}</h1>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <div>
+                        <i className="fa-solid fa-trash-can"></i>
+
+                        <i className="fa-solid fa-pen-to-square"></i>
+                    </div>
                 </div>
             </section>
         </div>
