c36b4e6173c8ab81c877ad2de1a5243983e63424
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-28 15:14:14 -0300

    mario

--- src/pages/usuario/naoEncontrado/index.jsx
+++ src/pages/usuario/naoEncontrado/index.jsx
@@ -1,9 +1,11 @@
 import './index.scss'
+import { Link } from 'react-router-dom';
 
 export default function NaoEncontrado() {
     return (
         <div className='pagina-nao-encontrado'>
-            <img className='NaoEnc' src="assets/images/MapaSobreMim.jpeg" alt="Mapa Mundi" />
+            <img className='NaoEnc' src="assets/images/NaoEnc.jpg" alt="Mapa Mundi" />
+            <div class="item"><Link to='/'><a href="">Voltar a Pagina Inicial</a></Link></div>
         </div>
     )
 }
