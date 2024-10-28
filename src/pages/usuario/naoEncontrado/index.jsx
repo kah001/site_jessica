3f08339b4dc567323274634558ca6aec8df1631d
Mario <ra44742733894@acaonsfatima.org.br>
2024-10-28 14:41:41 -0300

    inicio da pagina 404

--- src/pages/usuario/naoEncontrado/index.jsx
+++ src/pages/usuario/naoEncontrado/index.jsx
@@ -3,7 +3,7 @@ import './index.scss'
 export default function NaoEncontrado() {
     return (
         <div className='pagina-nao-encontrado'>
-            
+            <img className='NaoEnc' src="assets/images/MapaSobreMim.jpeg" alt="Mapa Mundi" />
         </div>
     )
 }
