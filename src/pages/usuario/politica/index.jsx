02e69d48ef3d025d4c8aa828d334ba490c8b20a0
kahvicente <karinasilvavicente007@gmail.com>
2024-10-31 11:34:19 -0300

    pagina da politica de privacidade quase pronta

--- src/pages/usuario/politica/index.jsx
+++ src/pages/usuario/politica/index.jsx
@@ -9,7 +9,7 @@ export default function PoliticaPrivacidade() {
             <Cabecalho />
 
             <section className='infos'>
-                <h1>{i18n.t('policy.title')}</h1>
+                <h1 className='titulo'>{i18n.t('policy.title')}</h1>
 
                 <ol>
                     <li><a href="#bem-vindo" className='link'>{i18n.t('policy.topic1')}</a></li>
@@ -18,18 +18,10 @@ export default function PoliticaPrivacidade() {
                     <li><a href="#informacoes-protegidas" className='link'>{i18n.t('policy.topic4')}</a></li>
                 </ol>
 
-
                 <section id='bem-vindo' className='bloco'>
                     <h1>Bem-vindo(a)!</h1>
                     <br />
                     <p>Aqui você terá acesso à política de privacidade dessa plataforma, tendo informações de quais informações suas são coletadas, como e para que fim as utilizamos e também como as suas informações são protegias com segurança. É importante que você leia esta política, mas aqui abaixo, está um breve resumo:</p>
-
-                    <div className='resumo'>
-                        <ul>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </div>
                 </section>
 
                 <section id='informacoes-coletadas' className='bloco'>
@@ -41,13 +33,13 @@ export default function PoliticaPrivacidade() {
                 <section id='informacoes-utilizadas' className='bloco'>
                     <h1>Como suas informações são utilizadas</h1>
                     <br />
-                    <p></p>
+                    <p>Todas essas informações que foram coletadas são utilizadas pela profissional a fim de oferecer um serviço personalizado aos seus clientes. Ou seja, todas as informações fornecidas por você são armazenadas e utilizadas </p>
                 </section>
 
                 <section id='informacoes-protegidas' className='bloco'>
                     <h1>Como suas informações são protegidas</h1>
                     <br />
-                    <p></p>
+                    <p>Utilizamos algumas medidas para ajudar a proteger as suas informações. Todas as uas informações são armazenadas, e utilizamos criptografia, para protegê-las, para que apenas a profissional tenha acesso a elas, a fim de prestar um serviço mais personalizado aos seus clientes. Ou seja, é imposto um controle de acesso administrativo, para limitar quem terá acesso e quem não terá acesso às suas informações.</p>
                 </section>
             </section>
 
